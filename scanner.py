78c2168e8262e8a08bec3e787a8c923d93050906
Rounit <rounit.singh0710@gmail.com>
2026-02-24 22:10:45 +0530

    header

---
+++ scanner.py
@@ -0,0 +1,9 @@
+# Name:
+# Roll No:
+# Course: Image Processing & Computer Vision
+# Unit:
+# Assignment Title: Smart Document Scanner & Quality Analysis System
+# Date:
+
+print("Welcome to Smart Document Scanner & Quality Analysis System")
+print("This system analyzes sampling and quantization effects on document quality.")
